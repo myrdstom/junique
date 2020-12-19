ca36b8f5b89340d2d0e90144d21972c43c4f3ce9
myrdstom <nserekopaul@gmail.com>
2020-12-19 20:48:57 +0300

    Add required fields

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import RegisterView from "./components/registration";
+import './App.css'
 
 
 function App() {
--- src/components/registration/index.js
+++ src/components/registration/index.js
@@ -1,58 +1,73 @@
-import React from 'react';
-import Register from './register';
-import {useFormik} from 'formik';
+import React from "react";
+import Register from "./register";
+import { useFormik } from "formik";
 import './registration.styles.scss';
-import * as Yup from 'yup';
-
+import * as Yup from "yup";
 
 const RegisterView = () => {
-    const formik = useFormik({
-        initialValues: { vorname: '', nachname: '', geburtsdatum: '', password: '', tag:'', monat:'', jahr:''},
-        validationSchema: Yup.object({
-            vorname: Yup.string()
-                .required('Vorname erforderlich'),
-            nachname: Yup.string()
-                .required('Nachname erforderlich'),
-            tag: Yup.string()
-                .required('ungültiges Geburtsdatum'),
-            monat: Yup.string()
-                .required('ungültiges Geburtsdatum'),
-            jahr: Yup.string()
-                .required('ungültiges Geburtsdatum'),
-            password: Yup.string()
-                .required('password required'),
-        }),
-        onSubmit: (values) => {
-            const { vorname, nachname, email, password } = values;
-            return {
-                campaignUuid: '46aa3270-d2ee-11ea-a9f0-e9a68ccff42a',
-                data: {
-                    vorname,
-                    nachname,
-                    email,
-                    password,
-                },
-            }
-        },
-    });
+  const formik = useFormik({
+    initialValues: {
+      checked: false,
+      vorname: "",
+      nachname: "",
+      tag: "",
+      monat: "",
+      jahr: "",
+      strabe: "",
+      nr: "",
+      plz: "",
+      ort: "",
+    },
+    validationSchema: Yup.object({
+      vorname: Yup.string().required("Vorname erforderlich"),
+      nachname: Yup.string().required("Nachname erforderlich"),
+      tag: Yup.number().typeError('geburtsdatum akzeptiert nur zahlen').required("ungültiges Geburtsdatum"),
+      monat: Yup.number().typeError('geburtsdatum akzeptiert nur zahlen').required("ungültiges Geburtsdatum"),
+      jahr: Yup.number().typeError('geburtsdatum akzeptiert nur zahlen').required("ungültiges Geburtsdatum"),
+      strabe: Yup.string().required("straße erforderlich"),
+      nr: Yup.string().required("straße erforderlich"),
+      plz: Yup.string().required("postleitzahl erforderlich"),
+      ort: Yup.string().required("postleitzahl erforderlich"),
+    }),
+    onSubmit: (values) => {
+      const {
+        vorname,
+        nachname,
+        tag,
+        monat,
+        jahr,
+      } = values;
+      return {
+        vorname,
+        nachname,
+        tag,
+        monat,
+        jahr,
+      };
+    },
+  });
 
-    return (
-        <div>
-            <Register
-                vorname={formik.values.vorname}
-                onChange={formik.handleChange}
-                nachname={formik.values.nachname}
-                tag={formik.values.tag}
-                monat={formik.values.monat}
-                jahr={formik.values.jahr}
-                password={formik.values.password}
-                onSubmit={formik.handleSubmit}
-                onBlur={formik.handleBlur}
-                errors={formik.errors}
-                touched={formik.touched}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Register
+          checked={formik.values.checked}
+        vorname={formik.values.vorname}
+        onChange={formik.handleChange}
+        nachname={formik.values.nachname}
+        tag={formik.values.tag}
+        monat={formik.values.monat}
+        jahr={formik.values.jahr}
+        strabe={formik.values.strabe}
+        nr={formik.values.nr}
+        plz={formik.values.plz}
+        ort={formik.values.ort}
+        onSubmit={formik.handleSubmit}
+        onBlur={formik.handleBlur}
+        errors={formik.errors}
+        touched={formik.touched}
+      />
+    </div>
+  );
 };
 
 export default RegisterView;
